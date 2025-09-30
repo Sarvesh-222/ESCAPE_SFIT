3187317c54d02bc23c136218198c91d1169c5509
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-09-30 23:09:30 +0530

    Added laser Modes

--- Scripts/LaserGameScripts/laser_beam_new.gd
+++ Scripts/LaserGameScripts/laser_beam_new.gd
@@ -2,7 +2,7 @@ extends Area3D
 var init_positon_y:=0.0
 var init_positon_x:=0.0
 
-@export_group("MODES(Select Only One)")
+@export_group("MODES(SEE EDITOR DESCRIPTION)")
 @export var up_down:=true
 @export var left_right:=false
 @export var rotate:=false
@@ -10,25 +10,36 @@ var init_positon_x:=0.0
 @export_group("ATTRIBUTES")
 @export var range:=50
 @export var moveSpeed:=10.0
+@export var rotateSpeed:=1.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#set initial positions
 	init_positon_y=position.y
 	init_positon_x=position.x
+	
+	#set animationSpeed
+	animation_player.speed_scale=rotateSpeed
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(up_down):
+	if(up_down):				#ONLY VERTICAL
 		UpDown(delta)
-	elif left_right:
+	elif left_right:			#ONLY HORIZONTAL
 		LeftRight(delta)
+	elif rotate:  			#ONLY ROTATE
+		animation_player.play("RotateLaser")
 	pass
 
 var isMovingUp:=true
 func UpDown(delta:float):
+	if(rotate): #ROTATE WITH UPDOWN 
+		animation_player.play("RotateLaser")
 	
 	if(isMovingUp):   #Up movement
 		if(position.y< init_positon_y+range):
@@ -45,6 +56,10 @@ func UpDown(delta:float):
 			
 var isMovingLeft:=false
 func LeftRight(delta:float):
+	if(rotate):
+		animation_player.play("RotateLaser")
+	
+	
 		if !isMovingLeft:  #Right Movement
 			if position.x<init_positon_x+range:
 				position.x+=moveSpeed*delta
