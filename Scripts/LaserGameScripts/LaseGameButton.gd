c2b746f705f60ad63dfef1901adecfa4da8addda
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-10-02 00:14:04 +0530

    button

---
+++ Scripts/LaserGameScripts/LaseGameButton.gd
@@ -0,0 +1,25 @@
+extends Area3D
+
+var playerInRange:=false;
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var main_button: MeshInstance3D = $MainButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(playerInRange and Input.is_key_pressed(KEY_E)):
+		print("Lasers Disabled")
+		playerInRange=false
+
+
+func _on_body_entered(body: Node3D) -> void:
+	playerInRange=true
+	#main_button.get_surface_override_material(0).e
+
+
+func _on_body_exited(body: Node3D) -> void:
+	playerInRange=false
