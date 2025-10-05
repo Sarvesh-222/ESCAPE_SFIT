ebc83e91dd7916dcb883dd8b533eed752ac69190
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-10-05 22:39:41 +0530

    Modified the Gamebutton scripr to support multiple games

--- Scripts/LaserGameScripts/GameButton.gd
+++ Scripts/LaserGameScripts/GameButton.gd
@@ -4,7 +4,13 @@ var playerInRange:=false;
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var main_button: MeshInstance3D = $MainButton
 
+@export var terminal:PackedScene
 
+@export_group("MODE (SELECT ONLY ONE)")
+@export var laserGame:=false
+@export var terminalGame:=false
+
+signal buttonPressed(body:CharacterBody3D)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,17 +19,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if(playerInRange and Input.is_key_pressed(KEY_E)):
-		print("Lasers Disabled")
-		playerInRange=false
+			buttonPressed.emit()
+			playerInRange=false
+	#if (laserGame):
+		#
+		#
+	#
+	#elif (terminalGame):
+		#
+		#if(playerInRange and Input.is_key_pressed(KEY_E)):
+			#print("Terminal Enabled")
+			#get_tree().current_scene.add_child(terminal.instantiate())
+			#playerInRange=false
+		
 
-
-func _on_body_entered(body: Node3D) -> void:
+func _on_body_entered(body: CharacterBody3D) -> void:
 	playerInRange=true
 	animation_player.play("ButtonGlow")
 	#main_button.get_surface_override_material(0).e
 
 
-func _on_body_exited(body: Node3D) -> void:
+func _on_body_exited(body: CharacterBody3D) -> void:
 	animation_player.play("ButtonGlow_off")
 	playerInRange=false
+
+	
+	
+
+
+func _on_button_pressed():
+	if terminalGame:
+		print("Terminal Enabled")
+		# Instantiate the terminal
+		var terminal_instance = terminal.instantiate()
+		# Add it to the main scene
+		get_tree().current_scene.add_child(terminal_instance)
+	elif laserGame:
+		print("LASERS DISABLED")
