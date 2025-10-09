31ed13ce8f378e076654b6f4e86ae316a157b8bb
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-10-09 18:17:08 +0530

    blocks:)

--- Scripts/BlockGame/puzzle_block.gd
+++ Scripts/BlockGame/puzzle_block.gd
@@ -1,4 +1,6 @@
 extends Node3D
+@export_group("Block Properties( Degree= n*90)")
+@export var correctRotationDegree:=0 
 
 @onready var block: CSGBox3D = $Area3D/CSGBox3D
 @onready var area3d: Area3D = $Area3D
@@ -11,14 +13,13 @@ var rotation_speed := 180.0 # degrees per second
 var blockSelected:=false
 @onready var label_3d: Label3D = $Area3D/CSGBox3D/Label3D
 
-signal BlockSelected(selected)
 func _ready() -> void:
 	area3d.input_event.connect(_on_area3d_input_event)
+	block.rotation_degrees.z=[0,90,180,270,360].pick_random() #initial rotation (random)
 
 func _process(delta: float) -> void:
 	label_3d.text=str(blockSelected)
-	#if(blockSelected and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
-		#blockSelected=false
+
 	
 	if(blockSelected):
 		rotateBlock(delta)
@@ -48,10 +49,9 @@ func _on_area3d_input_event(camera: Camera3D, event: InputEvent, position: Vecto
 	# Detect left mouse click
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		blockSelected=true
-		emit_signal("BlockSelected",blockSelected)
+
 		
 # Called when you click anywhere else
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			blockSelected = false
-			emit_signal("BlockSelected",blockSelected)
