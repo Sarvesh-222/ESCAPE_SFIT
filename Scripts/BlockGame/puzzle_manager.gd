08e94f3a1f203027ead7ce68ea0be2d43193db6d
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-10-10 12:28:47 +0530

    hexaBlocksAdded

--- Scripts/BlockGame/puzzle_manager.gd
+++ Scripts/BlockGame/puzzle_manager.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	allBlocksAlligned=true
 	for block in blocks:
-		var csgBlock:CSGBox3D=block.get_node("Area3D/CSGBox3D")
+		var csgBlock:CSGPolygon3D=block.get_node("Area3D/CSGPolygon3D")
 		# Check if this block is not aligned
 		if csgBlock.rotation_degrees.z != block.correctRotationDegree:
 			allBlocksAlligned = false
@@ -20,4 +20,4 @@ func _process(delta: float) -> void:
 	if(allBlocksAlligned):
 		#print("WIN")
 		for block in blocks:
-			block.get_node("Area3D/CSGBox3D/Label3D").text="Win"
+			block.get_node("Area3D/CSGPolygon3D/Label3D").text="Win"
